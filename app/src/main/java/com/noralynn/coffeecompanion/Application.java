af4c68d2ee3c359727f3f18c7d3f8e7a8556534c
nvalletta <nora.valletta@gmail.com>
2017-01-28 12:25:10 -0700

    Removes Application file; no need to extend

--- app/src/main/java/com/noralynn/coffeecompanion/Application.java
+++
@@ -1,9 +0,0 @@
-package com.noralynn.coffeecompanion;
-
-public class Application extends android.app.Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-}
